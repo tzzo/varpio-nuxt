a2c877c751a98e78c70fe65cbfca4a0416bfc6e3
Konstantinos Tzoulas <konsta@singa.com>
2021-09-19 20:13:14 +0300

    jess2

---
+++ dist/_nuxt/static/1632071577/tuotteet/frisee/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632071577",layout:"default-nav-footer",error:a,serverRendered:true,routePath:"\u002Ftuotteet\u002Ffrisee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},_img:{"/_ipx/q_90,s_602x451/img/products/frisee.jpg":"\u002F_nuxt\u002Fimage\u002F21d497.jpg"}}}(null));
