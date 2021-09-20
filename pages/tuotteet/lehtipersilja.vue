3f8ea75641530b2a00f8571cece27ba167d4facc
Konstantinos Tzoulas <konsta@singa.com>
2021-09-20 20:15:39 +0300

    jess

--- pages/tuotteet/lehtipersilja.vue
+++ pages/tuotteet/lehtipersilja.vue
@@ -3,7 +3,7 @@
     article.product
       .left
         h1.title Lehtipersilja
-        p 
+        p Erittäin monikäyttöinen mausteyrtti kaikkeen ruuanlaittoon. Kokeile vaikka keittoihin tai kastikkeisiin. Persiljalla on lisäksi lukuisia terveysvaikutuksia etenkin tuoreena nautittuna; se muun muassa raikastaa hengityksen ja auttaa ruuansulatusvaivoihin ja sisältää paljon mineraaleja ja antioksidantteja. Säilytä jääkaapissa tai ikkunalaudalla ja muista kastella.
       .right
         nuxt-img(preset="detail" width="602" height="451" src="/img/products/lehtipersilja.jpg")
 </template>
--- pages/tuotteet/tomaatti-rasia.vue
+++ pages/tuotteet/tomaatti-rasia.vue
@@ -3,7 +3,9 @@
     article.product
       .left
         h1.title Tomaatti rasia
-        p
+        p Erittäin suosittu tomaattimme on tavallista maukkaampi. Lajikevalinnat on tehty maku edellä ja tomaatit kerätään aina kypsänä. Tomaatti sisältää paljon lykopeenia, jonka on todettu ehkäisevän monia sairauksia. Säilytä huoneenlämmössä.
+
+
       .right
         nuxt-img(preset="detail" width="602" height="451" src="/img/products/tomaatti-rasia.jpg")
 </template>
